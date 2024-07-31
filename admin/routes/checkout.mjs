f7fb780bbbfa6456c577b09a3445a62d00539f8a
Minhdo0402 <109321527+Minhdo0402@users.noreply.github.com>
2024-07-31 10:26:09 +0700

    Update checkout.mjs

--- admin/routes/checkout.mjs
+++ admin/routes/checkout.mjs
@@ -30,8 +30,8 @@ router.post("/checkout", async (req, res) => {
       line_items: extractingItems,
       mode: "payment",
       success_url:
-        "https://ecommerce-2024-new-client.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",
-      cancel_url: "https://ecommerce-2024-new-client.vercel.app/cancel",
+        "https://ecommerce-2024-new.vercel.app/success?session_id={CHECKOUT_SESSION_ID}",
+      cancel_url: "https://ecommerce-2024-new.vercel.app/cancel",
       metadata: {
         email,
       },
