66b849e1250aa15ff3084477bf5d3f7b83082a74
Minhdo0402 <109321527+Minhdo0402@users.noreply.github.com>
2024-07-31 11:37:12 +0700

    final code

--- client/config.ts
+++ client/config.ts
@@ -1,28 +1,27 @@
 // config.ts
 interface Config {
-  baseUrl: string;
-}
-
-const checkConfig = (server: string): Config | Record<string, never> => {
-  let config: Config | Record<string, never> = {};
-  switch (server) {
-    case "production":
-      config = {
-        baseUrl: "https://ecommercedeploy-nine.vercel.app", // Update with your actual production URL
-      };
-      break;
-    case "local":
-      config = {
-        baseUrl: "http://localhost:8000",
-      };
-      break;
-    default:
-      config = {}; // Explicitly set as an empty object
-      break;
+    baseUrl: string;
   }
-  return config;
-};
+  
+  const checkConfig = (server: string): Config | Record<string, never> => {
+    let config: Config | Record<string, never> = {};
+    switch (server) {
+      case "production":
+        config = {
+          baseUrl: "https://ecommercedeploy-nine.vercel.app", // Update with your actual production URL
+        };
+        break;
+      case "local":
+        config = {
+          baseUrl: "http://localhost:8000",
+        };
+        break;
+      default:
+        config = {}; // Explicitly set as an empty object
+        break;
+    }
+    return config;
+  };
   
   export const selectServer = "production";
   export const config = checkConfig(selectServer) as Config;
-  
