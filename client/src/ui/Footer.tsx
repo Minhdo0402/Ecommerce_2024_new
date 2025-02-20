e35dd653f3610c0319759a6a4e00b8960f21100f
Minhdo0402 <109321527+Minhdo0402@users.noreply.github.com>
2025-02-20 21:14:15 +0700

    Update Footer.tsx

--- client/src/ui/Footer.tsx
+++ client/src/ui/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="mt-10">
             <FooterTop />
             <Container className="flex flex-col md:flex-row items-center gap-4 justify-between">
-                <p>@2024 E-commerce solutions. All rights reserved.</p>
+                <p>@2025 E-commerce solutions. All rights reserved.</p>
                 <img src={payment} alt="payment-img" className="object-cover" />
             </Container>
         </div>
